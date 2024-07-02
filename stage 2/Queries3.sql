5a3ad1136a2fd6a103330744912def9e6167b9fb
Rephael Riahi <126706531+rephael100@users.noreply.github.com>
2024-07-02 17:25:17 +0300

    Update Queries3.sql

--- stage 2/Queries3.sql
+++ stage 2/Queries3.sql
@@ -3,5 +3,4 @@ FROM Department d,
      (SELECT * FROM Position WHERE PositionID BETWEEN 1 AND 20) p, 
      MedicalSpecialty ms
 WHERE d.DepartmentID = p.DepartmentID
-  AND p.SpecialtyID = ms.SpecialtyID
-GROUP BY d.DepartmentName, p.PositionName, ms.SpecialtyName;
+  AND p.SpecialtyID = ms.SpecialtyID;
